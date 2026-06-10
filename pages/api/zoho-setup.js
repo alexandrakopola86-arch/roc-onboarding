5ea8eeedf8436434c250564faece840276561c45
Alexandra Kopola <alexandrakopola86@gmail.com>
2026-06-10 23:49:45 +0300

    Debug: get team folders

--- pages/api/zoho-setup.js
+++ pages/api/zoho-setup.js
@@ -7,9 +7,10 @@ export default async function handler(req, res) {
 
   try {
     const accessToken = await getAccessToken();
+    const teamId = "343ekec77de0592f34fbfbd3703a6e83bfdbb";
 
-    // Get user's own WorkDrive info
-    const r1 = await fetch("https://workdrive.zoho.eu/api/v1/users/me", {
+    // Get team folders
+    const r1 = await fetch(`https://workdrive.zoho.eu/api/v1/teams/${teamId}/folders`, {
       headers: { Authorization: "Zoho-oauthtoken " + accessToken },
     });
     const d1 = await r1.json();
