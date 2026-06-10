5ef580f7953f8ea5147c58fb19ef0e680eca1bb6
Alexandra Kopola <alexandrakopola86@gmail.com>
2026-06-10 23:53:08 +0300

    Setup: create folders using known parent ID

--- pages/api/zoho-setup.js
+++ pages/api/zoho-setup.js
@@ -7,15 +7,22 @@ export default async function handler(req, res) {
 
   try {
     const accessToken = await getAccessToken();
-    const teamId = "343ekec77de0592f34fbfbd3703a6e83bfdbb";
+    const parentId = "56pih6ac45089d8c34554882220a829d09656";
 
-    // Get team folders
-    const r1 = await fetch(`https://workdrive.zoho.eu/api/v1/teams/${teamId}/folders`, {
-      headers: { Authorization: "Zoho-oauthtoken " + accessToken },
+    const folders = ["Αγρότης", "Συνεταιρισμός", "Εταιρεία"];
+    const createdFolders = {};
+
+    for (const folderName of folders) {
+      const folder = await createFolder(accessToken, parentId, folderName);
+      createdFolders[folderName] = folder.id;
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Folders created! Add these to Vercel env vars",
+      parentId,
+      folderIds: createdFolders,
     });
-    const d1 = await r1.json();
-
-    return res.status(200).json({ debug: d1 });
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
